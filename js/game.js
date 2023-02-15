4bfddb461a546094c60710f01ebf824962c31339
Laura <Lausnclu@hotmail.com>
2023-02-15 17:49:07 +0100

    funcion para validar jugada ganadora

--- js/game.js
+++ js/game.js
@@ -33,3 +33,21 @@ const PressClick = (position) =>{
     }
 }
 
+// funcion para validar ganador
+let letra = ""
+const checkWinner = () =>{
+    // let letra = ""
+    let letras = ["X","O"]
+    for(a=0; a<letras.length; a++){
+        letra = letras[a]
+
+        if(board[0] == letra && board[1] == letra && board[2]== letra){MostrarGanador(letra)}
+        if(board[3] == letra && board[4] == letra && board[5]== letra){MostrarGanador(letra)}
+        if(board[6] == letra && board[7] == letra && board[8]== letra){MostrarGanador(letra)}
+        if(board[0] == letra && board[3] == letra && board[6]== letra){MostrarGanador(letra)}
+        if(board[1] == letra && board[4] == letra && board[7]== letra){MostrarGanador(letra)}
+        if(board[2] == letra && board[5] == letra && board[8]== letra){MostrarGanador(letra)}
+        if(board[0] == letra && board[4] == letra && board[8]== letra){MostrarGanador(letra)}
+        if(board[2] == letra && board[4] == letra && board[6]== letra){MostrarGanador(letra)}
+    }
+}
