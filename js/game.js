7bed186da0adab9ed63e7c657a226e59fbae5604
Laura <Lausnclu@hotmail.com>
2023-02-15 17:46:46 +0100

    condicion para limitar los turnos a tres para cada jugador

--- js/game.js
+++ js/game.js
@@ -3,39 +3,33 @@ const PlayerName = () => {
     document.getElementById("P1").innerHTML = sessionStorage.getItem("name1")
     document.getElementById("P2").innerHTML = sessionStorage.getItem("name2")
 }
-
 PlayerName();
-
-
-
-// jugadores 
-const firstPlayer = "X";
-const secondPlayer = "O";
 let board = [0,0,0,0,0,0,0,0,0];
 //inicia el juego con X
 let turn = 'X';     
-
+let counter = 0;
 // funcion para cambiar de turno
 const changeTurn = () =>{
     //si el turno esta en X lo cambia a O y al reves
-    turn = (turn == 'X') ? 'O' : 'X'; 
-}
+        turn = (turn == 'X') ? 'O' : 'X';
+};
+
 //  funcion para marcar la celda seleccionada
 const markCell = (id) =>{     //esta funcion debe recibir el id donde clickeo
-    if(board[id] === 0){    //si la celda esta vacia realizaremos lo siguiente
+   if(board[id] === 0){    //si la celda esta vacia realizaremos lo siguiente
         board[id]= turn              //almacenamos el turno sea la X o O
         document.getElementById(id).innerHTML = turn //insertamos el turno en la celda
-        changeTurn()
+        
     }
 }
-
- //  funcion al dar click sobre la celda
+//llamamos a la funcion de marcar celda
 
 const PressClick = (position) =>{
-    markCell(position)               //llamamos a la funcion de marcar celda
+    if(counter<6){
+        markCell(position) 
+        checkWinner()
+        changeTurn()
+        counter++
+    }
 }
-
-
-
-
 
